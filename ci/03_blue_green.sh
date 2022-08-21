0085fb4803bf40aee7d3b8cecdbb5785f0e8b8aa
Luiz Mandico <luizmandico@MacBook-Air-de-Mandico.local>
2022-08-21 18:58:18 -0300

    New Helms

---
+++ ci/03_blue_green.sh
@@ -0,0 +1,16 @@
+currentSlot=`(helm get values --all yourReleaseName | grep -Po '  productionSlot: \K.*')`
+if [ "$currentSlot" == "blue" ]; then
+    newSlot="green"
+else
+    newSlot="blue"
+fi
+
+
+deploymentOption=$newSlot.enabled=true
+helm upgrade yourReleaseName repo/blue-green --set $deploymentOption --reuse-values
+
+deploymentOption=productionSlot=$newSlot
+helm upgrade releasename repo/blue-green --set $deploymentOption --reuse-values
+
+deploymentOption=$oldSlot.enabled=false
+helm upgrade yourReleaseName repo/blue-green --set $deploymentOption --reuse-values
